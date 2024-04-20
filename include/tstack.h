72bbbbd193700e77f2445b3c0d7049159473960b
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-04-20 11:24:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,41 +4,47 @@
 
 template<typename T, int size>
 class TStack {
-    private:
-        T *stackArray;
-        int top;
-    public:
-        TStack() {
-            stackArray = new T[size];
-            top = -1;
+private:
+    T *stackArray;
+    int topIndex;
+
+ public:
+    TStack() {
+        stackArray = new T[size];
+        topIndex = -1;
+    }
+
+    ~TStack() {
+        delete[] stackArray;
+    }
+
+    void push(T item) {
+        if (topIndex != size - 1) {
+            topIndex++;
+            stackArray[topIndex] = item;
         }
-        
-        ~TStack() {
-            delete[] stackArray;
+    }
+
+    T pop() {
+        if (topIndex != -1) {
+            T element = stackArray[topIndex];
+            topIndex--;
+            return element;
+        } else {
+            return T();
         }
-        
-        void push(T value) {
-            if (top == size - 1) {
-                std::cout << "Stack overflow" << std::endl;
-                return;
-            }
-            stackArray[++top] = value;
+    }
+
+    T get() const {
+        if (topIndex == -1) {
+            return T();
+        } else {
+            return stackArray[topIndex];
         }
-        
-        void pop() {
-            if (top == -1) {
-                std::cout << "Stack is empty" << std::endl;
-                return;
-            }
-            top--;
-        }
-        
-        T get() {
-            return stackArray[top];
-        }
-        
-        bool isEmpty() {
-            return (top == -1);
-        }
+    }
+
+    bool isempty() const {
+        return topIndex == -1;
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
