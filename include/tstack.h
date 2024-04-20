f12ec530954f84c37d0d087c1275ac42632066a6
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-04-20 11:13:37 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,43 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+    private:
+        T *stackArray;
+        int top;
+    public:
+        TStack() {
+            stackArray = new T[size];
+            top = -1;
+        }
+        
+        ~TStack() {
+            delete[] stackArray;
+        }
+        
+        void push(T value) {
+            if (top == size - 1) {
+                std::cout << "Stack overflow" << std::endl;
+                return;
+            }
+            stackArray[++top] = value;
+        }
+        
+        void pop() {
+            if (top == -1) {
+                std::cout << "Stack is empty" << std::endl;
+                return;
+            }
+            top--;
+        }
+        
+        T get() {
+            return stackArray[top];
+        }
+        
+        bool isEmpty() {
+            return (top == -1);
+        }
+};
 };
 
 #endif  // INCLUDE_TSTACK_H_
